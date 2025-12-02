6b1f3036520af63b5ef80eb157194cbd0c41ea1a
NabidKabir <nabid.kabir312@gmail.com>
2025-12-01 20:02:58 -0500

    fixed database.sql/added app.py, requirements.txt
    Must add database to local mysql in order to go further. Can do this by creating a database within mysql named "CS631_Final" and running
    
    mysql -u root -p -D CS631_Final < database.sql
    
    in terminal.

--- database.sql
+++ database.sql
@@ -1,14 +1,14 @@
---================================================================
---DIVISION
---================================================================
+-- ================================================================
+-- DIVISION
+-- ================================================================
 CREATE TABLE Division(
     Division_Name VARCHAR(100) PRIMARY KEY,
     Head_Employee_No INT UNIQUE
 );
 
---================================================================
---DEPARTMENT
---================================================================
+-- ================================================================
+-- DEPARTMENT
+-- ================================================================
 CREATE TABLE Department(
     Department_Name VARCHAR(100) PRIMARY KEY,
     Budget DECIMAL (12, 2),
@@ -18,9 +18,9 @@ CREATE TABLE Department(
     FOREIGN KEY (Division_Name) REFERENCES Division(Division_Name)
 );
 
---================================================================
---BUILDING
---================================================================
+-- ================================================================
+-- BUILDING
+-- ================================================================
 CREATE TABLE Building(
     Building_Code VARCHAR(10) PRIMARY KEY,
     Building_Name VARCHAR(200),
@@ -28,9 +28,9 @@ CREATE TABLE Building(
     Cost DECIMAL(12, 2)
 );
 
---================================================================
---PROJECT
---================================================================
+-- ================================================================
+-- PROJECT
+-- ================================================================
 CREATE TABLE Project(
     Project_No INT PRIMARY KEY,
     Budget DECIMAL(12, 2),
@@ -38,17 +38,17 @@ CREATE TABLE Project(
     Date_Ended DATE
 );
 
---================================================================
---EMPLOYEE_TITLE
---================================================================
+-- ================================================================
+-- EMPLOYEE_TITLE
+-- ================================================================
 CREATE TABLE EmployeeTitle(
     Title VARCHAR(100) PRIMARY KEY,
     Salary DECIMAL(12,2)
 );
 
---================================================================
---EMPLOYEE
---================================================================
+-- ================================================================
+-- EMPLOYEE
+-- ================================================================
 CREATE TABLE Employee(
     Employee_No INT PRIMARY KEY,
     Employee_Name VARCHAR(200),
@@ -56,13 +56,13 @@ CREATE TABLE Employee(
     Title VARCHAR(100),
     Department_Name VARCHAR(100),
 
-    FOREIGN KEY Title REFERENCES Employee_Title(Title),
-    FOREIGN KEY Department_Name REFERENCES Department(Department_Name)
+    FOREIGN KEY (Title) REFERENCES EmployeeTitle(Title),
+    FOREIGN KEY (Department_Name) REFERENCES Department(Department_Name)
 );
 
---================================================================
---ROOM
---================================================================
+-- ================================================================
+-- ROOM
+-- ================================================================
 CREATE TABLE Room (
     Office_Number INT PRIMARY KEY,
     Square_Feet INT,
@@ -72,9 +72,9 @@ CREATE TABLE Room (
     FOREIGN KEY (Building_Code) REFERENCES Building(Building_Code)
 );
 
---================================================================
---EMPLOYEE_ROOM
---================================================================
+-- ================================================================
+-- EMPLOYEE_ROOM
+-- ================================================================
 CREATE TABLE EmployeeRoom (
     Employee_No INT PRIMARY KEY,
     Room_Number INT UNIQUE,
@@ -84,9 +84,9 @@ CREATE TABLE EmployeeRoom (
 );
 
 
---================================================================
---DEPARTMENT_ROOM
---================================================================
+-- ================================================================
+-- DEPARTMENT_ROOM
+-- ================================================================
 CREATE TABLE DepartmentRoom (
     Department_Name VARCHAR(100),
     Room_Number INT,
@@ -96,14 +96,14 @@ CREATE TABLE DepartmentRoom (
     FOREIGN KEY (Room_Number) REFERENCES Room(Office_Number)
 );
 
---================================================================
---EMPLOYEE_PROJECT
---================================================================
+-- ================================================================
+-- EMPLOYEE_PROJECT
+-- ================================================================
 CREATE TABLE EmployeeProject (
     Employee_No INT,
-    Project_Number INT,
-    PRIMARY KEY (Employee_No, Project_Number),
+    Project_No INT,
+    PRIMARY KEY (Employee_No, Project_No),
 
     FOREIGN KEY (Employee_No) REFERENCES Employee(Employee_No),
-    FOREIGN KEY (Project_Number) REFERENCES Project(Project_Number)
+    FOREIGN KEY (Project_No) REFERENCES Project(Project_No)
 );
