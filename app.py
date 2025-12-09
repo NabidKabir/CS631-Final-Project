51aae092617aa31b8d5198f8e663d608fa8fe483
NabidKabir <nabid.kabir312@gmail.com>
2025-12-08 19:08:26 -0500

    added many things

--- app.py
+++ app.py
@@ -1,5 +1,20 @@
 from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
+from dotenv import load_dotenv
 from datetime import datetime
 import os
 
+app = Flask(__name__)
+
+load_dotenv()
+
+db_user = os.environ["DB_USER"]
+db_pass = os.environ["DB_PASS"]
+db_name = os.environ["DB_NAME"]
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "mysql://{db_user}:{db_pass}@localhost/{db_name}".format(
+    db_user = db_user, db_pass = db_pass, db_name = db_name
+)
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db = SQLAlchemy(app)
